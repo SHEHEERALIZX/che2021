e7dd93bfc843814da6e0b2da6ab777a78516d163
SHEHEERALIZX <nssheheerali@gmail.com>
2021-02-26 15:06:56 +0530

    26/02/2021

---
+++ Python/factorial.py
@@ -0,0 +1,9 @@
+fact = 1
+
+
+num = int(input("Input the number to find the factorial of a number :"))
+
+for res in range(1,num + 1):
+    fact = fact * res
+
+print(fact)    
---
+++ Python/prime_or_not.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number to find prime or not : "))
+
+
+if (num2 == 0):
+    print("Not Prime")
+else:
+    print("Prime")
