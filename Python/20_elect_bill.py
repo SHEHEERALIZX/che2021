dece7a094b6d8c95966ccb0f287b67de011a225f
SHEHEERALIZX <nssheheerali@gmail.com>
2021-02-26 11:25:30 +0530

    final commit

--- Python/20_elect_bill.py
+++ Python/20_elect_bill.py
@@ -5,11 +5,11 @@ unit_consumed = int(input("Enter the units consumed by customer : "))
 if (unit_consumed <= 200):
     charge = 0.5 * unit_consumed
 elif (unit_consumed > 200) and (unit_consumed <=400):
-    charge = 100 + 0.65 * unit_consumed
+    charge = 100 + 0.65 * (unit_consumed - 200)
 elif (unit_consumed > 400) and (unit_consumed <= 600):
-    charge = 230 + 0.80 * unit_consumed    
+    charge = 230 + 0.80 * (unit_consumed - 400)    
 elif (unit_consumed > 600):
-    charge = 390 + 1.0 * unit_consumed
+    charge = 390 + 1.0 * ( unit_consumed - 600)
 
 
 print("Your Electricity Bill for consuming ",unit_consumed, " unit is : ", charge)       
